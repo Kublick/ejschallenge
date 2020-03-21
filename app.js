ba6f7aed117c03d991c2ef05439ec8ceacaf6bc9
Max Blancarte <blancarte@gmail.com>
2020-03-21 13:19:59 -0700

    added lowcase and postfunctionality

--- app.js
+++ app.js
@@ -6,47 +6,77 @@ const ejs = require("ejs");
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const _ = require('lodash');
+
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// Global Variaable
+
+let posts = [];
 
 
 // home
-app.get("/", function (req,res) {
-    res.render("home.ejs", {
-    startingContent: homeStartingContent
-    });
- });
+app.get("/", function (req, res) {
+  console.log(posts);
+  res.render("home.ejs", {
+    startingContent: homeStartingContent,
+    posts: posts
+  });
 
- //about
- app.get("/about", function (req,res) {
+});
+
+//about
+app.get("/about", function (req, res) {
   res.render("about.ejs", {
-  aboutContent: aboutContent
+    aboutContent: aboutContent
   });
 });
 
- //Contact
- app.get("/contact", function (req,res) {
+//Contact
+app.get("/contact", function (req, res) {
   res.render("contact.ejs", {
-  contactContent: contactContent
+    contactContent: contactContent
   });
 });
 
 //Compose
-app.get("/compose", function (req,res) {
+app.get("/compose", function (req, res) {
   res.render("compose.ejs");
 });
 
-app.post("/compose", function(req, res) {
-  console.log(req.body.postBody);
-  
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.newTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
+//multiroute
+app.get("/posts/:postName", function (req, res) {
+  
+  const requestedTitle = _.lowerCase(req.params.postName);
+  console.log(requestedTitle);
+  
+  posts.forEach(element => { 
+  element.title 
+  const lowCase = _.lowerCase(element.title);
+  if (lowCase === requestedTitle) {
+    console.log("Success");
+  } else { console.log("no url");}
+  });
+  
+})
+
+
 // Server Listener
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
